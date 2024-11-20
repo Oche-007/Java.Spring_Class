4e29df8d2454eff6b1a5291caffb022154a4a273
UGBAH-NDUKA DAVID KING <130305040+Oche-007@users.noreply.github.com>
2024-11-20 20:46:47 +0100

    spring model

---
+++ Main/Java /service /service.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+import com.example.demo.model.User;
+
+public interface UserService {
+    User createUser(User user);
+    User updateUser(User user);
+    List<User> getAllUsers();
+}
