d09b67bcc3d80adfb9d75efc88f67b14c81ec392
UGBAH-NDUKA DAVID KING <130305040+Oche-007@users.noreply.github.com>
2024-11-20 20:42:42 +0100

    spring model

---
+++ Main/Java /Repository/Repository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {    
+    User findByFullName(String fullName);
+}
