6ec9e50fe2a307cc289c82c66adce25c5ae47413
UGBAH-NDUKA DAVID KING <130305040+Oche-007@users.noreply.github.com>
2024-11-20 21:03:39 +0100

    Subject.java

---
+++ Main/Java /Subject.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Subject {
+    @Id
+    private Long id;
+
+    private String subjectTitle;
+    private String subjectCode;
+
+    
+
+    
+}
